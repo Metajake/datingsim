9e5cba6db22385f59ab2dcbdc77e847c8cc6391c
Metajake <jaynoco@gmail.com>
2015-03-20 11:36:52 -0400

    adjusted textbox element

--- graphicsobj.py
+++ graphicsobj.py
@@ -8,20 +8,27 @@ class Graphics(object):
         self.DARKGREEN = (16, 88, 4)
         self.GREEN = (0, 255, 0)        
         
-        self.day_overlay = PyDark.ui.Overlay(name="day_overlay", parent=self.engine.day_scene, size=self.engine.window_size, color=self.GREEN, endcolor=self.DARKGREEN, position=(0, 0))
+        self.day_overlay = PyDark.ui.Overlay(
+            name="day_overlay",
+            parent=self.engine.day_scene,
+            size=self.engine.window_size,
+            color=self.GREEN,
+            endcolor=self.DARKGREEN,
+            position=(0, 0))
         
         self.text_entry = PyDark.ui.TextBox(
             name="text_entry",
-            position=(0, 150),
-            center=True,
-            default_image="assets/input.png",)
+            position=(0, 250),
+            default_image="assets/input.png")
         
-        self.submit = PyDark.ui.Button(name="submit", position=(0, 225),
-                                  on_press=self.submit_pressed,
-                                  center=True,
-                                  default_image="assets/button.png",
-                                  image_hover="assets/button_hover.png",
-                                  image_selected="assets/button_hover.png")
+        self.submit = PyDark.ui.Button(
+            name="submit",
+            position=(0, 225),
+            on_press=self.submit_pressed,
+            center=True,
+            default_image="assets/button.png",
+            image_hover="assets/button_hover.png",
+            image_selected="assets/button_hover.png")
 
     def submit_pressed(self, event):
         print self.text_entry.text
